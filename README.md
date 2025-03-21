befa0cd416e19d37cdb4a2d3c7fabb6fb491f3ce
keshavim <keshavim2004@gmail.com>
2025-03-20 20:47:35 -0500

    some updates

---
+++ README.md
@@ -0,0 +1,12 @@
+A assembler I made for the mips language to learn about assembly and how to parse data from files.
+This is suposed to act similer to the MARS IDE for mips.
+It has a smaller instruction set then MARS but the instructions it has should give the same hex numbers when assembled
+
+to run, or clone this project and compile with an ide
+
+Then give the asm / text files for it to assemble
+
+it can take multiple files at once and will compile each into two files, data_filename and text_filename.
+
+It also has a "-o" flag which reads the next arg as a directory and output the files to that directory instead of the directory
+of the original file.
--- src/Main.java
+++ src/Main.java
@@ -10,71 +10,7 @@ import java.util.Objects;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    private static final String GREEN = "\u001B[32m";
-    private static final String RESET = "\u001B[0m";
 
-    private static void instruction_test(){
-        String filename = "instructions_test";
-        Assembler asm = new Assembler();
-        try(BufferedReader br = new BufferedReader(new FileReader(filename))){
-            String line;
-            while((line = br.readLine()) != null){
-
-                long expected = Mips.parseNumber("0x"+line.substring(0, 8));
-
-                long[] result = asm.assemble_word(line.substring(9));
-
-                if(result[0] == expected){
-                    System.out.println(GREEN +"Passes: "+line + RESET);
-                }
-                else{
-                    System.err.println("Failed: "+line);
-                    System.exit(1);
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    static void test_file() throws IOException {
-        FileAssembler fasm = new FileAssembler();
-
-        String testdir = "Tests/";
-
-        File test = new File(testdir);
-        File[] files = test.listFiles();
-
-        assert files != null;
-        fasm.assembleFile(testdir + files[0].getName());
-        fasm.assembleFile(testdir + files[1].getName());
-        fasm.assembleFile(testdir + files[2].getName());
-
-
-//
-
-
-
-    }
-    public static boolean compareFiles(String file1Path, String file2Path) throws IOException {
-        try (BufferedReader reader1 = new BufferedReader(new FileReader(file1Path));
-             BufferedReader reader2 = new BufferedReader(new FileReader(file2Path))) {
-
-            String line1 = reader1.readLine();
-            String line2 = reader2.readLine();
-
-            while (line1 != null && line2 != null) {
-                if (!line1.equals(line2)) {
-                    System.out.println(line1);
-                    System.out.println(line2);
-                    return false;
-                }
-                line1 = reader1.readLine();
-                line2 = reader2.readLine();
-            }
-            return true;
-        }
-    }
 
     public static void main(String[] args)  {
 
